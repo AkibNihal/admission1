78a226740f5d444adec28b31075dd2f0e6369d5b
AkibNihal <79849459+AkibNihal@users.noreply.github.com>
2026-02-15 22:00:40 +0600

    Update Bulb.cs

--- admission/Task2/Bulb.cs
+++ admission/Task2/Bulb.cs
@@ -6,19 +6,20 @@ namespace Task2
 {
     public class Bulb
     {
-        public event Action On;
-        public event Action Off;
+        public event Action<string> On;
+        public event Action<string> Off;
 
         public void TurnON()
         {
             Console.WriteLine("Bulb On");
-            On?.Invoke();
+            On?.Invoke("bulb on string");
         }
-
+        
         public void TurnOff()
         {
             Console.WriteLine("Bulb Off");
-            Off?.Invoke(); 
+            Off?.Invoke("bulb off string"); 
         }
     }
 }
+
