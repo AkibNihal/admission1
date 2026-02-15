9bb44ecc51e9ac62949aa21d7ff46ba834b8d870
AkibNihal <79849459+AkibNihal@users.noreply.github.com>
2026-02-15 22:01:12 +0600

    Update Program.cs

--- admission/Task2/Program.cs
+++ admission/Task2/Program.cs
@@ -5,8 +5,9 @@ Console.WriteLine("Hello, World!");
 
 Bulb bulb = new Bulb();
 
-bulb.On += () => Console.WriteLine("event turn on");
-bulb.Off += () => Console.WriteLine("event turn off");
+bulb.On += (stat) => Console.WriteLine("event turn on");
+bulb.Off += (stat) => Console.WriteLine("event turn off");
 
 bulb.TurnON();
 bulb.TurnOff();
+
